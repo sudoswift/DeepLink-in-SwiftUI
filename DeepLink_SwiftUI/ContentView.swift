7e3c40bdad4af2f116d400853e88e5184b4b8ce3
sudoswift <sudoswift@gmail.com>
2021-05-27 13:30:59 +0900

    Initial Commit

---
+++ DeepLink_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DeepLink_SwiftUI
+//
+//  Created by izvern on 2021/05/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DeepLink_SwiftUI/DeepLink_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepLink_SwiftUIApp.swift
+//  DeepLink_SwiftUI
+//
+//  Created by izvern on 2021/05/27.
+//
+
+import SwiftUI
+
+@main
+struct DeepLink_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
